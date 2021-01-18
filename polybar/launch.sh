c9c659ee301e1f230d546819b495b5ecd6b39f0e
calvillain <hobbes102@gmail.com>
2021-01-18 15:10:33 -0500

    updates

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
+# Launch iebar
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar i3bar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
